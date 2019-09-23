76513b7feb2aeb44a28f53a39edeb093ea77a84a
Lily Best <lily.m.best@gmail.com>
2019-09-23 15:52:34 -0500

    Initial commit

---
+++ io.js
@@ -0,0 +1,16 @@
+// io.js
+var io = require('socket.io')();
+
+// Listen for new connections from clients (socket)
+io.on('connection', function (socket) {
+    //new code below
+    socket.on('add-circle', function (data) {
+        io.emit('add-circle', data);
+    });
+});;
+
+
+
+// io represents socket.io on the server - let's export it
+module.exports = io;
+
---
+++ public/javascripts/app.js
@@ -0,0 +1,56 @@
+var circles = document.getElementById('circles');
+var initials = '';
+//connect to socket.io
+var socket = io(); 
+console.log(socket) 
+// listen to the server for the `add-circle` event
+socket.on('add-circle', function (data) {
+    addCircle(data);
+});
+
+circles.addEventListener('click', function(evt) {
+  // replace current line of code with this code
+  socket.emit('add-circle', {
+      initials: initials,
+      x: evt.clientX,
+      y: evt.clientY,
+      dia: randomBetween(10,100),
+      rgba: getRandomRGBA()
+  });
+});
+
+document.getElementsByTagName('button')[0].addEventListener('click', function() {
+  circles.innerHTML = '';
+});
+
+do {
+  initials = getInitials();
+} while (initials.length < 2 || initials.length > 3);
+
+function getInitials() {
+  var input = prompt("Please enter your initials");
+  return input ? input.toUpperCase() : '';
+}
+
+function addCircle({x, y, dia, rgba, initials}) {
+  var el = document.createElement('div');
+  el.style.left = x - Math.floor(dia / 2 + 0.5) + 'px';
+  el.style.top = y - Math.floor(dia / 2 + 0.5) + 'px';
+  el.style.width = el.style.height = dia + 'px';
+  el.style.backgroundColor = rgba;
+  el.style.fontSize = Math.floor(dia / 3) + 'px';
+  el.style.color = 'white';
+  el.style.textAlign = 'center';
+  el.style.lineHeight = dia + 'px';
+  el.innerHTML = initials;
+  circles.appendChild(el);
+}
+
+function randomBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getRandomRGBA() {
+  return ['rgba(', randomBetween(0, 255), ',', randomBetween(0, 255), ',',
+    randomBetween(0, 255), ',', randomBetween(2, 10) / 10, ')'].join('');
+}
